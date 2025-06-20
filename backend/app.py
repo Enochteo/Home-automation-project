2bc7c7ef883831d9f35638c81178d21d81a23e83
Enochteo <enochowoade@gmail.com>
2025-06-20 00:54:42 -0500

    add website routes

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,27 @@
+from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__)
+
+sensor_data = {"temperature": 0, "Humidity": 0, "light":0}
+device_states = {"light": False}
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/update", methods=["POST"])
+def update_sensor():
+    data = request.json
+    sensor_data.update(data)
+    return jsonify({"status": "success"}), 200
+
+@app.route("/status")
+def det_status():
+    return jsonify(sensor_data | device_status)
+
+
+@app.route("/toggle/<device>")
+def toggle_device(device):
+    if device in device_states:
+        device_states[device] = not device_states[device]
+    return jsonify({device: device_states[device]})
